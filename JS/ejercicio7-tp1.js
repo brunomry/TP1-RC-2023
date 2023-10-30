5799ce89595843c5af9fb9f7025fee1b98489732
brunomry <brunomadozzo@gmail.com>
2023-10-30 02:33:04 -0300

    Resolucion de ejercicio n° 7 - tp1

---
+++ JS/ejercicio7-tp1.js
@@ -0,0 +1,41 @@
+document.write(`<h2>Resolución de Ejercicio N°7 - TP1</h2>`);
+
+let numero1 = parseInt(prompt("Ingrese el primer número: "));
+let numero2 = parseInt(prompt("Ingrese el segundo número: "));
+let numero3 = parseInt(prompt("Ingrese el tercer número: "));
+
+//SOLUCION 1 - APLICANDO CONDICIONAL IF-ELSE
+
+document.write(`<h3>- Solución n°1 aplicando condicional if-else:</h3>`);
+
+if(numero1 === numero2 && numero1 === numero3){
+  document.write(`<p>Los 3 números ingresados son iguales</p>`);
+}else if(numero1 > numero2 && numero1 > numero3){
+  document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${numero1}</p>`);
+}else if(numero2 > numero3){
+  document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${numero2}</p>`);
+}else{
+  document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${numero3}</p>`);
+}
+
+//SOLUCION 2 - APLICANDO OPERADOR TERNARIO
+
+document.write(`<h3>- Solución n°2 aplicando operador ternario:</h3>`);
+
+(numero1 === numero2 && numero1 === numero3) 
+  ? document.write(`<p>Los 3 números ingresados son iguales</p>`)
+  : (numero1 > numero2 && numero1 > numero3) 
+    ? document.write(`<p>El mayor entre número ${numero1} y número ${numero2} es el número ${numero1}</p>`)
+    : (numero2 > numero3)
+      ? document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${numero2}</p>`)
+      : document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${numero3}</p>`)
+
+//SOLUCION 3 - APLICANDO EL OBJETO MATH
+
+document.write(`<h3>- Solución n°3 aplicando objeto Math:</h3>`);
+
+if(numero1 === numero2 && numero1 === numero3){
+  document.write(`<p>Los 3 números ingresados son iguales</p>`);
+}else{
+  document.write(`<p>El mayor entre número ${numero1}, número ${numero2} y número ${numero3} es el número ${Math.max(numero1,numero2,numero3)}</p>`);
+}
