c868d02e3b4f236f643cbfe9deb7bb2f57784729
brunomry <brunomadozzo@gmail.com>
2023-10-29 22:23:41 -0300

    Resolucion de ejercicio 5 - tp1

---
+++ JS/ejercicio5-tp1.js
@@ -0,0 +1,5 @@
+document.write(`<h2>Resolución de Ejercicio N°5 - TP1</h2>`);
+
+let numero1 = parseInt(prompt("Ingrese el primer número: "));
+let numero2 = parseInt(prompt("Ingrese el segundo número: "));
+document.write(`La suma entre número ${numero1} y número ${numero2} es igual a: ${numero1+numero2}`);
