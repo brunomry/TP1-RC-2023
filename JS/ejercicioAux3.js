fb4201726d3e34996c82652e4f88b59c4859c997
brunomry <brunomadozzo@gmail.com>
2023-10-29 21:31:14 -0300

    Resolucion de ejercicio auxiliar n°3

---
+++ JS/ejercicioAux3.js
@@ -0,0 +1,7 @@
+document.write(`<h2>Resolución de ejercicio auxiliar n°3</h2>`);
+document.write(`El resultado se muestra por consola`);
+
+let gradosCelsius = 30;
+let gradosFahrenheit = gradosCelsius * 9/5 + 32;
+
+console.log(`${gradosCelsius} grados celsius equivalen a ${gradosFahrenheit} grados fahrenheit`);
